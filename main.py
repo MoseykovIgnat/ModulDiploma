49967a802ce800790db0087c1ac4fcd4eaeb866d
Ignat Moseykov <Moseyjr99@gmail.com>
2021-02-18 16:13:03 +0700

    Added functions which can be used to close DB connection and cursor, DJANGODB con created

--- main.py
+++ main.py
@@ -16,20 +16,43 @@ def create_connect_to_db(config):
 
 class Getters(object):
     def __init__(self, time, source):
-        self.time = time
+        self.time_5sec = time  # example for 60 sec = 20
         self.source = source
 
 
 class Db_getter(Getters):
     def __init__(self, time, source):
-        super(Getters,self).__init__(time, source)
+        super(Getters, self).__init__(time, source)
         self.config = SQLParser.xxxdbrc.config(source)
         self.connection = create_connect_to_db(self.config)
         self.cursor = self.connection.cursor()
+
+    def db_close_connection(self):
+        self.cursor.close()
+        self.connection.close()
 
     def hello(self):
         print("all is right")
 
 
+
+
+
+
+def close_cursor_connection(cursor, connection):
+    cursor.close()
+    connection.close()
+    print("Connection was closed")
+
+
+# Создаем одинарное подключение для БД Django
+djangodb_connection = create_connection_djangodb()
+djangodb_cursor = djangodb_connection.cursor()
+
+# Основная работа
 clbdb = Db_getter(5, 'clb')
 clbdb.hello()
+clbdb.db_close_connection()
+
+# Закрываем соединение в конце работы пограммы
+close_cursor_connection(djangodb_cursor, djangodb_connection)

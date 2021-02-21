e0900e2cd8b7dba01029ece73d5c05f4fd3ea916
Ignat Moseykov <Moseyjr99@gmail.com>
2021-02-21 12:51:52 +0700

    Added getter for channels for every variable, added getter for DC1, added alias for TemdBase, added res dict for DJango Db!

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import MySQLdb
 import SQLParser.xxxdbrc
 import time
+
+
+def create_alias_to_tem_db():
+    d = dict()
+    inp = open("Alias.txt", 'r')
+    for i in inp.readlines():
+        key, val = i.strip().split(' ')
+        d[key] = val
+    return d
 
 
 def create_connect_to_db(config):
@@ -12,6 +23,16 @@ def create_connect_to_db(config):
                                  port=config["port"],
                                  charset='utf8')
 
+    return connection
+
+
+def create_connection_djangodb():
+    connection = MySQLdb.connect(host='sndfarm08-00.sndonline',
+                                 user='scadmin',
+                                 passwd='sc1s1mp0rtnt',
+                                 db='sctestdb',
+                                 port=3306,
+                                 charset='utf8')
     return connection
 
 
@@ -38,12 +59,13 @@ class Manager(object):
             try:
                 config = SQLParser.xxxdbrc.config(name)
                 connection = create_connect_to_db(config)
-                cursor = connection.cursor()
+                cursor = connection.cursor(MySQLdb.cursors.DictCursor)
                 self.data[name] = Connection_db(name, config, connection, cursor)
                 return self.data[name]
             except:
                 print("I can't connect to db: " + name)
-
+        else:
+            return self.data[name]
 
     def refresh_dict(self):
         for c in self.data.values():
@@ -76,58 +98,124 @@ class Db_getter(Getters):
         self.db.connection.close()
 
 
-class Clb_db_getter(Db_getter):
-    def __init__(self, db_manager):
-        super(Clb_db_getter, self).__init__(10, 'clb', db_manager)
+# class Clb_db_getter(Db_getter):
+#     def __init__(self, db_manager, interval):
+#         super(Clb_db_getter, self).__init__(interval, 'clb', db_manager)
+#
+#     def cld_db_print(self):
+#         if self.interval_time <= self.expired_time:
+#             print("Cld_worked (it's working every 10 sec)")
+#             self.expired_time = 0
+#
+#
+# class Adm_db_getter(Db_getter):
+#     def __init__(self, db_manager, interval):
+#         super(Adm_db_getter, self).__init__(interval, 'adm', db_manager)
+#
+#     def adm_db_print(self):
+#         if self.interval_time <= self.expired_time:
+#             print("adm_worked (it's working every 5 sec)")
+#             self.expired_time = 0
 
-    def cld_db_print(self):
+
+class channels_tem_db_getter(Db_getter):
+    def __init__(self, db_manager):
+        super(channels_tem_db_getter, self).__init__(3600, 'tem', db_manager)
+        self.channels_tem_db = dict()
+
+    def get_channels_tem_db(self):
+        return self.channels_tem_db
+
+    def getter(self, alias):
+        res = dict()
+        query = "select * from v_dir where parent=%s and name = %s"
+        for key, value in alias.iteritems():
+            a = value[::-1]
+            a = a.split('/', 1)[-1]
+            args = (a[::-1], value.split('/')[-1])
+            self.db.cursor.execute(query, args)
+            data = self.db.cursor.fetchall()
+            for a in data:
+                res[key] = a['id']
+        self.channels_tem_db = res
+
+
+class DC1_tem_db_getter(Db_getter):
+    def __init__(self, db_manager):
+        super(DC1_tem_db_getter, self).__init__(5, 'tem', db_manager)
+
+    def getter(self, channel):
         if self.interval_time <= self.expired_time:
-            print("Cld_worked (it's working every 10 sec)")
+            self.db.cursor.execute("select c_value from t_lastinteger where c_channel=%s" % channel)
+            data = self.db.cursor.fetchall()
+            for res in data:
+                DC1 = res['c_value']
+            print("Got DC1")
             self.expired_time = 0
+            return DC1
+        else:
+            pass
 
 
-class Adm_db_getter(Db_getter):
-    def __init__(self, db_manager):
-        super(Adm_db_getter, self).__init__(5, 'adm', db_manager)
+# class test_db_getter(Db_getter):
+#     def __init__(self, db_manager, interval):
+#         super(test_db_getter, self).__init__(interval, 'asdg', db_manager)
 
-    def adm_db_print(self):
-        if self.interval_time <= self.expired_time:
-            print("adm_worked (it's working every 5 sec)")
-            self.expired_time = 0
-
-
-class test_db_getter(Db_getter):
-    def __init__(self, db_manager):
-        super(test_db_getter, self).__init__(5, 'asdg', db_manager)
-
-def create_connection_djangodb():
-    connection = MySQLdb.connect(host='asdf-00.asdg',
-                                 user='asdg',
-                                 passwd='asdg',
-                                 db='asdg',
-                                 port=3306,
-                                 charset='asdg')
-    return connection
-
-
+'''Создаем подключение к БД DJNAGO для сеттеров'''
 djangodb_connection = create_connection_djangodb()
 djangodb_cursor = djangodb_connection.cursor()
 
-manager_of_db = Manager()
-clbdb = Clb_db_getter(manager_of_db)
-admdb = Adm_db_getter(manager_of_db)
-test = test_db_getter(manager_of_db)
+alias_for_tem_db = {'LOCK': '/VEPP2K/STATUS/SND_INTERLOCK', 'FLT': '/SND/SCALERS/INTEGRALS/FLT',
+                    'FLTinc': '/SND/SCALERS/INCREMENTS/FLT', 'ST': '/SND/SCALERS/INTEGRALS/ST', 'E_laser': '/EMS/E',
+                    'dE_laser': '/EMS/DE', 'BEP_PMT': '/VEPP2K/STATUS/BEP_PMT', 'STinc': '/SND/SCALERS/INCREMENTS/ST',
+                    'shunt': '/VEPP2K/STATUS/VEPP_POWER_CURRENT', 'E_VEPP': '/VEPP2K/STATUS/VEPP_ENERGY',
+                    'E_NMR': '/VEPP2K/STATUS/E_NMR', 'L': '/SND/DERIVED/L', 'IL': '/SND/DERIVED/IL',
+                    'IProd': '/SND/DERIVED/IProd', 'DC1': '/SND/SCALERS/INTEGRALS/DC1',
+                    'DC1inc': '/SND/SCALERS/INCREMENTS/DC1', 'time': '/SND/SCALERS/INTEGRALS/TIME',
+                    'Tlive': '/SND/SCALERS/INTEGRALS/TIMELIVE', 'Tliveinc': '/SND/SCALERS/INCREMENTS/TIMELIVE',
+                    'E_PMT': '/VEPP2K/STATUS/VEPP_IE', 'P_PMT': '/VEPP2K/STATUS/VEPP_IP',
+                    'VEPP_FZ': '/VEPP2K/STATUS/VEPP_FZ', 'run': '/SND/SCALERS/RUN',
+                    'timeinc': '/SND/SCALERS/INCREMENTS/TIME', 'FLT1': '/SND/SCALERS/INTEGRALS/FLT1',
+                    'FLT1inc': '/SND/SCALERS/INCREMENTS/FLT1', 'etau': '/VEPP2K/STATUS/VEPP_E_TAU',
+                    'ptau': '/VEPP2K/STATUS/VEPP_P_TAU', 'fztau': '/VEPP2K/STATUS/VEPP_FZ_TAU',
+                    'ePMT': '/VEPP2K/CAS/VEPP/CURRENTS/EPMT', 'pPMT': '/VEPP2K/CAS/VEPP/CURRENTS/PPMT',
+                    'E0': '/VEPP2K/STATUS/VEPP_E0', 'setE': '/VEPP2K/STATUS/VEPP_SET_ENERGY',
+                    'ILSH': '/SND/DERIVED/ILSH', 'ILPT': '/SND/DERIVED/ILPT', 'k': '/SND/DERIVED/PWRFLT1',
+                    'sigma0': '/SND/DERIVED/SIGMAFLT1', 'fcosm': '/SND/DERIVED/CSMFLT1',
+                    'RMNAFLT': '/SND/DERIVED/RMNAFLT', 'E_EMS_DT': '/EMS/DT', 'Tklk': '/SND/CPS/KAS/1/007',
+                    'NMR_1M1': '/VEPP2K/STATUS/NMR_1M1', 'NMR_1M2': '/VEPP2K/STATUS/NMR_1M2',
+                    'NMR_2M1': '/VEPP2K/STATUS/NMR_2M1', 'NMR_2M2': '/VEPP2K/STATUS/NMR_2M2',
+                    'NMR_3M1': '/VEPP2K/STATUS/NMR_3M1', 'NMR_3M2': '/VEPP2K/STATUS/NMR_3M2',
+                    'NMR_4M1': '/VEPP2K/STATUS/NMR_4M1', 'NMR_4M2': '/VEPP2K/STATUS/NMR_4M2',
+                    'NMR_AVG': '/VEPP2K/STATUS/NMR_AVG', 'VEPP_RF_FREQ': '/VEPP2K/CAS/VEPP/RF/FREQ',
+                    'GENC': '/SND/SCALERS/INTEGRALS/GENC',
+                    'GENCinc': '/SND/SCALERS/INCREMENTS/GENC'}  # Создали словарь alias для путей (parent)
+manager_of_db = Manager()  # создали менеджера db
+tem_ch = channels_tem_db_getter(manager_of_db)
+tem_ch.getter(alias_for_tem_db)
+print(tem_ch.channels_tem_db)
+
+results = dict()
+
+# clbdb = Clb_db_getter(manager_of_db, 10)
+# admdb = Adm_db_getter(manager_of_db, 5)
+# test = test_db_getter(manager_of_db,5)
+DC1_getter = DC1_tem_db_getter(manager_of_db)
+DC1_getter2 = DC1_tem_db_getter(manager_of_db)
 
 while True:
     current_time = time.time()
-    clbdb.cld_db_print()
-    admdb.adm_db_print()
+    # clbdb.cld_db_print()
+    # admdb.adm_db_print()
+    results['dc1'] = DC1_getter.getter(tem_ch.get_channels_tem_db()['DC1'])
     time.sleep(5 - ((time.time() - current_time) % 60.0))
+    print(results)
     lead_time = time.time() - current_time
-    clbdb.update_time(lead_time)
-    admdb.update_time(lead_time)
+    DC1_getter.update_time(lead_time)
+    # clbdb.update_time(lead_time)
+    # admdb.update_time(lead_time)
 
 close_cursor_connection(djangodb_cursor, djangodb_connection)
-clbdb.db_close_connection()
-admdb.db_close_connection()
+# clbdb.db_close_connection()
+# admdb.db_close_connection()
 print("everything is working!")
